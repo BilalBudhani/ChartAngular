4d821cda07a45f7952880d449b61d8378212743b
BilalBudhani <bilalbudhani@gmail.com>
2013-10-20 20:31:23 +0530

    barchart added and some code refactoring

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
-angular.module('MyApp', ['ChartAngular']).
-  controller('MyCtrl', function($scope, $timeout){
+angular.module('MyChartApp', ['ChartAngular']).
+  controller('PieChartCtrl', function($scope, $timeout){
 
   $scope.chart_data = [55, 20, 13, 32, 5];
   $scope.chart_options = {
     title: {
-      font: "26px verdana"
+      font: "26px verdana",
+      x: 320,
+      y: 70
     },
     chart: {
     }
@@ -19,4 +21,26 @@ angular.module('MyApp', ['ChartAngular']).
     }, 2000);
   };
 
+}).
+controller('BarChartCtrl', function($scope, $timeout) {
+
+  $scope.chart_data = [[55, 20, 13, 32, 5, 1, 2, 10]];
+  $scope.chart_options = {
+    title: {
+      font: "12px sans-serif",
+      x: 160,
+      y: 10
+    },
+    chart: {
+    }
+  };
+  $scope.addVal = function addVal() {
+    $scope.chart_data[0].push(Math.floor(Math.random() * 100));
+  };
+
+  $scope.addTimedVal = function addTimedVal() {
+    $timeout(function() {
+      $scope.chart_data[0].push(Math.floor(Math.random() * 100));
+    }, 2000);
+  };
 });
--- src/directives/chartangular.js
+++ src/directives/chartangular.js
@@ -10,7 +10,6 @@ angular.module('ChartAngular', []).
   };
 
   function removeChart(chart) {
-    chart.clear();
     chart.remove();
   }
 
@@ -22,7 +21,7 @@ angular.module('ChartAngular', []).
     var r = Raphael(attrs.id);
     source = angular.copy(scope.source); // Prevents from getting changed by raphael
     if (attrs.title) {
-      r.text(320, 70, attrs.title).attr(scope.options.title);
+      r.text(scope.options.title.x, scope.options.title.y, attrs.title).attr(scope.options.title);
     }
     r.piechart(
       parseInt(attrs.width), 
@@ -51,6 +50,59 @@ angular.module('ChartAngular', []).
         createChart(scope, attrs);
       }, true);
 
+    }
+  };
+}).
+directive('barchart', function($log, $window){
+  var chart, source;
+  var default_options = {
+      title: {
+        font: "20px sans-serif"
+      },
+      chart: {}
+  };
+
+  function removeChart(chart) {
+    chart.remove();
+    $window.chart = chart;
+  }
+
+  function createChart(scope, attrs) {
+    if (angular.isUndefined(scope.options)) {
+      scope.options = default_options;
+    }
+
+    var r = Raphael(attrs.id);
+    source = angular.copy(scope.source); // Prevents from getting changed by raphael
+    if (attrs.title) {
+      r.text(scope.options.title.x, scope.options.title.y, attrs.title).attr(scope.options.title);
+    }
+    r.barchart(
+      parseInt(attrs.x), 
+      parseInt(attrs.y), 
+      parseInt(attrs.width),
+      parseInt(attrs.height), 
+      source, 
+      scope.options.chart);
+    
+    return r;
+  }
+    return {
+    restrict: 'E',
+    scope: {
+      source: '=',
+      options: '='
+    }, 
+    template: '<div></div>',
+    replace: true,
+    link: function(scope, element, attrs) {
+     var chart = createChart(scope, attrs);
+
+      scope.$watch('source', function(new_source, old_source) {
+        if (new_source === old_source) return ; // do nothing when the source is same
+        removeChart(chart);
+        createChart(scope, attrs);
+      }, true); 
     }
   };
 });

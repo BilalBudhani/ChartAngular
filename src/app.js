02d4c4b7c3cf12f08f293080b841361376a0cf11
BilalBudhani <bilalbudhani@gmail.com>
2013-10-28 12:21:15 +0530

    added dependencies for offline mode support and barchart directive done with example

--- src/app.js
+++ src/app.js
@@ -1,60 +1,19 @@
 angular.module('MyChartApp', ['ChartAngular']).
-  controller('PieChartCtrl', function($scope, $timeout){
+  controller('BarChartCtrl', function($scope, $timeout){
 
   $scope.chart_options = {
-    title: {
-      x: 320,
-      y: 70,
-      options: {
-        font: "26px verdana"
-      }
-    },
-    chart: {
-      width: 320,
-      height: 240,
-      radius: 150,
-      source: [55, 20, 13, 32, 5],
-      options: {}
-    }
-  };
-  $scope.addVal = function addVal() {
-    $scope.chart_options.chart.source.push(Math.floor(Math.random() * 100));
+    data: [
+      { y: '2006', a: 100, b: 90 },
+      { y: '2007', a: 75,  b: 65 },
+      { y: '2008', a: 50,  b: 40 },
+      { y: '2009', a: 75,  b: 65 },
+      { y: '2010', a: 50,  b: 40 },
+      { y: '2011', a: 75,  b: 65 },
+      { y: '2012', a: 100, b: 90 }
+    ],
+    xkey: 'y',
+    ykeys: ['a', 'b'],
+    labels: ['Series A', 'Series B']
   };
 
-  $scope.addTimedVal = function addTimedVal() {
-    $timeout(function() {
-      $scope.chart_options.chart.source.push(Math.floor(Math.random() * 100));
-    }, 1000);
-  };
-
-}).
-controller('BarChartCtrl', function($scope, $timeout) {
-
-  $scope.chart_options = {
-    title: {
-      x: 160,
-      y: 10,
-      options: {
-        font: "12px sans-serif"
-      }
-    },
-    chart: {
-      x: 10,
-      y: 10,
-      width: 300,
-      height: 220,
-      source: [[55, 20, 13, 32, 5, 1, 2, 10]],
-      options: {}
-    }
-  };
-
-  $scope.addVal = function addVal() {
-    $scope.chart_options.chart.source[0].push(Math.floor(Math.random() * 100));
-  };
-
-  $scope.addTimedVal = function addTimedVal() {
-    $timeout(function() {
-      $scope.chart_options.chart.source[0].push(Math.floor(Math.random() * 100));
-    }, 1000);
-  };
 });
--- src/directives/chartangular.js
+++ src/directives/chartangular.js
@@ -1,37 +1,8 @@
 angular.module('ChartAngular', []).
-  directive('piechart', function(){
-  
-  var chart, source;
-  var default_options = {
-      title: {
-        font: "20px sans-serif"
-      },
-      chart: {}
-  };
-
-  function removeChart(chart) {
-    chart.remove();
-  }
-
-  function createChart(scope, attrs) {
-    if (angular.isUndefined(scope.options)) {
-      scope.options = default_options;
-    }
-
-    var chart_opts = scope.options.chart;
-    var title_opts = scope.options.title;
-    var r = Raphael(attrs.id);
-
-    source = angular.copy(chart_opts.source); // Prevents from getting changed by raphael
-    r.text(title_opts.x, title_opts.y, attrs.title).attr(title_opts.options);
-
-    r.piechart(
-      parseInt(chart_opts.width), 
-      parseInt(chart_opts.height), 
-      parseInt(chart_opts.radius), 
-      source, 
-      chart_opts.options);
-    
+  directive('barchart', function(){
+  function createChart(el_id, options) {
+    options.element = el_id;
+    var r = new Morris.Bar(options);    
     return r;
   }
 
@@ -43,68 +14,7 @@ angular.module('ChartAngular', []).
     replace: true,
     template: '<div></div>',
     link: function link(scope, element, attrs) {
-      var chart = createChart(scope, attrs);
-
-      scope.$watch('options.chart.source', function(new_source, old_source) {
-        if (new_source === old_source) return ; // do nothing when the source is same
-        removeChart(chart);
-        createChart(scope, attrs);
-      }, true);
-
-    }
-  };
-}).
-directive('barchart', function($log, $window){
-  var chart, source;
-  var default_options = {
-      title: {
-        font: "20px sans-serif"
-      },
-      chart: {}
-  };
-
-  function removeChart(chart) {
-    chart.remove();
-    $window.chart = chart;
-  }
-
-  function createChart(scope, attrs) {
-    if (angular.isUndefined(scope.options)) {
-      scope.options = default_options;
-    }
-
-    var chart_opts = scope.options.chart;
-    var title_opts = scope.options.title;
-    var r = Raphael(attrs.id);
-
-    source = angular.copy(chart_opts.source); // Prevents from getting changed by raphael
-    r.text(title_opts.x, title_opts.y, attrs.title).attr(title_opts.options);
-
-    r.barchart(
-      parseInt(chart_opts.x), 
-      parseInt(chart_opts.y), 
-      parseInt(chart_opts.width),
-      parseInt(chart_opts.height), 
-      source, 
-      chart_opts.options);
-    
-    return r;
-  }
-    return {
-    restrict: 'E',
-    scope: {
-      options: '='
-    }, 
-    template: '<div></div>',
-    replace: true,
-    link: function(scope, element, attrs) {
-     var chart = createChart(scope, attrs);
-
-      scope.$watch('options.chart.source', function(new_source, old_source) {
-        if (new_source === old_source) return ; // do nothing when the source is same
-        removeChart(chart);
-        createChart(scope, attrs);
-      }, true); 
+      return createChart(attrs.id, scope.options);
     }
   };
 });

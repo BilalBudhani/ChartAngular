670cf9923fa3eefb39f0a452feaed26e7dc378d9
BilalBudhani <bilalbudhani@gmail.com>
2013-10-29 14:24:01 +0530

    line and donut directives with example added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 angular.module('MyChartApp', ['ChartAngular']).
-  controller('BarChartCtrl', function($scope, $timeout){
+  controller('BarChartCtrl', function($scope){
 
   $scope.chart_options = {
     data: [
@@ -14,6 +14,35 @@ angular.module('MyChartApp', ['ChartAngular']).
     xkey: 'y',
     ykeys: ['a', 'b'],
     labels: ['Series A', 'Series B']
+  };
+
+}).
+  controller('LineChartCtrl', function($scope){
+
+  $scope.chart_options = {
+    data: [
+      { y: '2006', a: 100, b: 90 },
+      { y: '2007', a: 75,  b: 65 },
+      { y: '2008', a: 50,  b: 40 },
+      { y: '2009', a: 75,  b: 65 },
+      { y: '2010', a: 50,  b: 40 },
+      { y: '2011', a: 75,  b: 65 },
+      { y: '2012', a: 100, b: 90 }
+    ],
+    xkey: 'y',
+    ykeys: ['a', 'b'],
+    labels: ['Series A', 'Series B']  
+  };
+
+}).
+  controller('DonutChartCtrl', function($scope){
+
+  $scope.chart_options = {
+    data: [
+      {label: "Download Sales", value: 12},
+      {label: "In-Store Sales", value: 30},
+      {label: "Mail-Order Sales", value: 20}
+    ]
   };
 
 });
--- src/directives/chartangular.js
+++ src/directives/chartangular.js
@@ -1,8 +1,9 @@
 angular.module('ChartAngular', []).
   directive('barchart', function(){
+  
   function createChart(el_id, options) {
     options.element = el_id;
-    var r = new Morris.Bar(options);    
+    var r = new Morris.Bar(options);
     return r;
   }
 
@@ -17,4 +18,46 @@ angular.module('ChartAngular', []).
       return createChart(attrs.id, scope.options);
     }
   };
-});
+}).
+  directive('linechart', function() {
+
+    function createChart(el_id, options) {
+      options.element = el_id;
+      var r = new Morris.Line(options);
+      return r;
+    }
+
+    return {
+      restrict: 'E',
+      scope:  {
+        options: '='
+      },
+      replace: true,
+      template: '<div></div>',
+      link: function(scope, element, attrs) {
+        return createChart(attrs.id, scope.options)
+      }
+    }
+
+  }).
+  directive('donutchart', function() {
+
+    function createChart(el_id, options) {
+      options.element = el_id;
+      var r = new Morris.Donut(options);
+      return r;
+    }
+
+    return {
+      restrict: 'E',
+      scope:  {
+        options: '='
+      },
+      replace: true,
+      template: '<div></div>',
+      link: function(scope, element, attrs) {
+        return createChart(attrs.id, scope.options)
+      }
+    }
+
+  });
